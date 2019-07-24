2df8bcaac287ac758c9058ce08113dc9075c648e
lathou <tholfou@gmail.com>
2019-07-24 11:58:12 +0200

    add build folder

---
+++ build/precache-manifest.04508cd12d2e35fbe072f05605994f1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96332390140380e32e10b2f999cf1c29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a70537fb157771864a37",
+    "url": "/static/css/main.ca0c3fa1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a70537fb157771864a37",
+    "url": "/static/js/main.18d04645.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
