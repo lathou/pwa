54f08def96dbe456785b63311fcbf405f1159921
lathou <tholfou@gmail.com>
2019-07-24 14:42:13 +0200

    update bg color

---
+++ build/precache-manifest.ff07f5fabd66f5504eb8719e0f6c3e3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e28d2412e065675b2e0238289c07f3d",
+    "url": "/pwa/build/index.html"
+  },
+  {
+    "revision": "d58a9956e3993fdf5e5d",
+    "url": "/pwa/build/static/css/main.0ff7ed1c.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pwa/build/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d58a9956e3993fdf5e5d",
+    "url": "/pwa/build/static/js/main.f3a66521.chunk.js"
+  },
+  {
+    "revision": "e18c76c3c2e2d902fe70",
+    "url": "/pwa/build/static/js/runtime~main.8a2311bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
